af10b991be351bd686d181981bde2226f4a18e2c
esteban <efornal@gmail.com>
2017-02-08 12:19:07 -0300

    added missing migrations file

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from app.models import Attribute
+from app.models import PossibleAttributeValue
+from app.models import Group
+from app.models import GroupAttributeValue
+from app.models import Node
+from app.models import NodeAttributeValue
+
+
 
 # Register your models here.
+admin.site.register( Attribute )
+admin.site.register( PossibleAttributeValue )
+admin.site.register( Group )
+admin.site.register( GroupAttributeValue )
+admin.site.register( Node )
+admin.site.register( NodeAttributeValue )
+
+
+
